386628dd29f10f1629e436f6f51fcd806c09e889
Jacob Torres <jacobangelo_torres@yahoo.com>
2024-01-17 22:44:21 -0800

    Refector to new files

--- general_functions.py
+++ general_functions.py
@@ -1,13 +1,7 @@
-import pandas as pd
-from sodapy import Socrata
 import boto3
-
-import pandas as pd
 from sodapy import Socrata
+import pandas as pd
 import psycopg2
-
-from psql_create_tables import create_business_data_table_txt, create_parking_data_table_txt, create_crime_data_table_txt
-
 
 def get_secret(secret_name, region):
 	print("stats are  {} {}".format(secret_name, region))
@@ -55,7 +49,6 @@ def connect_to_la_city_api(app_val_token, lacity_password):
 			password=lacity_password,
 			timeout=10
 	)
-
 	return client
 
 def get_data_from_la_city(client, url_suffix, limit_value):
@@ -100,54 +93,4 @@ def run_sql(conn, sql_to_run):
 		print(sql_to_run)
 		print(e)
 		exit(1)
-
-
-
-data_code_dictionary = {
-
-	"business": ["r4uk-afju", ["location_account", "naics", "location"]],
-
-	"parking_spot_loc": ["s49e-q6j2", ["spaceid", "latlng"]],
-
-	"parking_spot_archived": ["cj8s-ivry", ["SpaceID", "EventTime_Local", "EventTime_UTC", "OccupancyState"]], # will scrape separately
-
-	"parking_spot_realtime": ["e7h6-4a3e", ["spaceid", "eventtime", "occupancystate"]],
-
-	"crime": ["amvf-fr72", ["rpt_id", "lat", "lon", "arst_date"]]
-
-}
-
-psql_password = get_secret("psql_password_official", "us-east-1")
-
-app_val_token = get_secret("app_token_val_official", "us-east-1")
-lacity_password = get_secret("data_lacity_password_official", "us-east-1")
-client = connect_to_la_city_api(app_val_token, lacity_password)
-
-business_results = get_data_from_la_city(client, data_code_dictionary['business'][0], 10)
-show_data_sample(business_results, data_code_dictionary['business'][1])
-
-parking_spot_loc_results = get_data_from_la_city(client, data_code_dictionary['parking_spot_loc'][0], 10)
-show_data_sample(parking_spot_loc_results, data_code_dictionary['parking_spot_loc'][1])
-
-parking_spot_realtime_results = get_data_from_la_city(client, data_code_dictionary['parking_spot_realtime'][0], 10)
-show_data_sample(parking_spot_realtime_results, data_code_dictionary['parking_spot_realtime'][1])
-
-
-crime_results = get_data_from_la_city(client, data_code_dictionary['crime'][0], 10)
-show_data_sample(crime_results, data_code_dictionary['crime'][1])
-
-
-conn = connect_to_psql_db(psql_password)
-
-# create business table, parking table, crime table
-run_sql(conn, create_business_data_table_txt)
-
-run_sql(conn, create_parking_data_table_txt)
-
-run_sql(conn, create_crime_data_table_txt)
-
-
-
-
-
 
