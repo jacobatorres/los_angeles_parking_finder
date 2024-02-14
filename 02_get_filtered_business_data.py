6c7eb242ff98e71dd5e6d8be66690c9dcb100805
Jacob Torres <jacobangelo_torres@yahoo.com>
2024-02-13 21:29:27 -0800

    Use active businesses

--- 02_get_filtered_business_data.py
+++ 02_get_filtered_business_data.py
@@ -2,7 +2,7 @@ from general_functions import *
 
 data_code_dictionary = {
 
-	"business": ["r4uk-afju", ["location_account", "naics", "primary_naics_description", "location"]],
+	"business": ["6rrh-rzua", ["location_account", "naics", "primary_naics_description", "location"]],
 
 	"parking_spot_loc": ["s49e-q6j2", ["spaceid", "latlng"]],
 
@@ -20,17 +20,40 @@ app_val_token = get_secret("app_token_value", "us-east-1")
 lacity_password = get_secret("data_lacity_password_value", "us-east-1")
 client = connect_to_la_city_api(app_val_token, lacity_password)
 
-business_results = get_data_from_la_city(client, data_code_dictionary['business'][0], 100, 0, "location_account ASC")
-results_df = pd.DataFrame.from_records(business_results)
-print(results_df)
+# business_results = get_data_from_la_city(client, data_code_dictionary['business'][0], 100, 0, "location_account ASC")
+# results_df = pd.DataFrame.from_records(business_results)
+# print(results_df)
 
 
+limit_offset_counter = 1
 
 # https://dev.socrata.com/docs/queries/offset
-for i in range(0,100, 3):
-	business_results_2 = get_data_from_la_city(client, data_code_dictionary['business'][0], 3, i, "location_account ASC")
+for i in range(0,10, limit_offset_counter):
+	business_results_2 = get_data_from_la_city(client, data_code_dictionary['business'][0], limit_offset_counter, i, "location_account ASC")
 	results_df_2 = pd.DataFrame.from_records(business_results_2)
-	print(results_df_2)
+
+	mini_counter = 0
+	while mini_counter < limit_offset_counter:
+
+		try:
+			print(results_df_2.loc[mini_counter])
+			location_id = results_df_2.loc[mini_counter]['location_account']
+			lat = results_df_2.loc[mini_counter]['location_1']['latitude']
+			lng = results_df_2.loc[mini_counter]['location_1']['longitude']
+			print("{} {} {}".format(location_id, lat, lng))
+		except Exception as e:
+			print("skipping")
+
+		mini_counter += 1
+
+
+
+	insert_sql_statement = "INSERT INTO business_location (location_id, naics_code, lat, lng) VALUES (%s, %s, %s, %s);"
+	# params = (results_df.loc[i]['location_account'], lat, lng, event_time_utc, state)
+
+	# break
+	# print(csv_file + ": running sql...")
+	# run_sql(conn, insert_sql_statement, params)
 
 
 
