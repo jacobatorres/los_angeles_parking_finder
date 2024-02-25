6c73f04547f4bb1586294f9db57fda476104bc74
Jacob Torres <jacobangelo_torres@yahoo.com>
2024-02-24 18:31:52 -0800

    Make simple login app in django, only one user

--- webapp/website/views.py
+++ webapp/website/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages 
 
-# Create your views here.
+
+def home(request):
+	# if log in, show data, else just the outside
+
+	if request.method == "POST":
+		username = request.POST['username']
+		password = request.POST['password']
+
+		# authenticate
+		user = authenticate(request, username=username, password=password)
+
+		if user is not None:
+			login(request, user)
+			messages.success(request, "You're logged in!")
+			return redirect("home")
+		else:
+			messages.success(request, "Error when logging in, please try again")
+			return redirect("home")
+	
+	else:
+		# just see the gomr
+		return render(request, 'home.html', {})
+
+def logout_user(request):
+	logout(request)
+	messages.success(request, "You're logged out!")
+	return redirect('home')
